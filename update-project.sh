f937720992a87c744bbc9708c1df52415b264e1f
lombold <colin.disler@gmail.com>
2026-05-15 14:03:11 +0200

    feat: add script to sync with blueprint main branch

---
+++ update-project.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+git fetch blueprint main
+git checkout chore/sync-blueprint
+git merge blueprint/main || true
+
+read -p "Have you resolved all conflicts? (y/n) " -n 1 -r
+echo    # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git add .
+    git commit -m "chore: sync with blueprint main"
+    git checkout main
+    git merge chore/sync-blueprint
+
+    echo "All done! Don't forget to push your changes."
+else
+    git merge --abort
+    echo "Run this script again."
+fi
+exit 0
